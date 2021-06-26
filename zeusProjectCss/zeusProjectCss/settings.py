921888e72e68fc5e77b7e30ff6a1f1a377ef0a64
Zeus6768 <akaps6768@gmail.com>
2021-06-26 17:41:39 +0900

    0626 ongoing

--- zeusProjectCss/zeusProjectCss/settings.py
+++ zeusProjectCss/zeusProjectCss/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            'soundCloudHome/templates',
+            'zeusProjectCss/templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -120,6 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'zeusProjectCss', 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Default primary key field type
