f87af0651b377df1a41520299f2361b4414b38bd
Zeus6768 <akaps6768@gmail.com>
2021-06-28 01:34:50 +0900

    Working on Navbar

--- SoundCloud/discover/views.py
+++ SoundCloud/discover/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.urls.conf import path
+from django.urls.resolvers import URLPattern
 
 # Create your views here.
+def discover(request):
+    return render(request, "discover.html")
